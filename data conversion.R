a9aa897ce31d4f9466ad7c603b20ab98a34acf92
Vicellken <gulivecassie@gmail.com>
2019-04-07 12:25:55 +0800

    Update data conversion.R

--- data conversion.R
+++ data conversion.R
@@ -1,11 +1,11 @@
 # convert csv file to rds file and save it called 'df'
 
-df <- read.csv('uni_sci_year.csv')
-saveRDS(df, file = "uni_sci_year.rds")
-df <- readRDS("uni_sci_year.rds")
+df <- read.csv('df.csv')
+saveRDS(df, file = "df_new.rds")
+df_new <- readRDS("df_new.rds")
 
 # remove duplicated data entry
 new_uni <- uni[!duplicated(uni), ]
 
 # create csv file
-write.csv(df, file = "df.csv")
+write.csv(new_uni, file = "new_uni.csv")
